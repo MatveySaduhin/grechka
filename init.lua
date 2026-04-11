6332faa8b97b399fb0ea825eac029901cb6dabe7
Harrier du Bois <sadyukh@gmail.com>
2026-04-11 16:27:30 +0400

    feat: add plugin.lua for managing plugins with vim.pack; install and enable folke/tokyonight.nvim by default

--- init.lua
+++ init.lua
@@ -7,9 +7,11 @@ require 'keymaps'
 
 require 'auto-commands'
 
+require 'plugins'
+
 local ok, _ = pcall(require, 'colorscheme')
 if not ok then
-  vim.cmd.colorscheme 'default'
+  vim.cmd.colorscheme 'tokyonight-moon'
 end
 
 -- vim: ts=2 sts=2 sw=2 et
