1156c7aeac0f3a2bf9b5b29bd92e235719913727
Harrier du Bois <sadyukh@gmail.com>
2026-04-12 20:07:50 +0400

    feat: add nvim-mini/mini.move plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,7 @@ return {
     vim.pack.add({
         "https://github.com/folke/tokyonight.nvim",
         "https://github.com/folke/which-key.nvim",
+        "https://github.com/echasnovski/mini.nvim",
         {
             src = "https://github.com/nvim-treesitter/nvim-treesitter",
             version = "main",
