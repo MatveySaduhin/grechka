b4dd36a99f551c01e017153be265ecb3703d722f
Harrier du Bois <sadyukh@gmail.com>
2026-04-12 19:21:22 +0400

    feat: add and configure nvim-treesitter plugin

--- lua/auto-commands.lua
+++ lua/auto-commands.lua
@@ -20,4 +20,11 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,5 +2,9 @@ return {
     vim.pack.add({
         "https://github.com/folke/tokyonight.nvim",
         "https://github.com/folke/which-key.nvim",
+        {
+            src = "https://github.com/nvim-treesitter/nvim-treesitter",
+            version = "main",
+        },
     })
 }
