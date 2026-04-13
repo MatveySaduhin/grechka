28498746c1ab4fb1875ab3fc1d437b8e35b789bc
Harrier du Bois <sadyukh@gmail.com>
2026-04-13 20:11:22 +0400

    feat: add autocommands for LSP

--- lua/auto-commands.lua
+++ lua/auto-commands.lua
@@ -21,4 +21,17 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local buf = args.buf
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition', buffer = buf })
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration', buffer = buf })
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Show references', buffer = buf })
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover info', buffer = buf })
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action', buffer = buf })
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol', buffer = buf })
+        vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format document', buffer = buf })
+    end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
