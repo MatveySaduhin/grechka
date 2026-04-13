e22b550ff0f1bb81599fef81e2c8947c127afd47
Harrier du Bois <sadyukh@gmail.com>
2026-04-13 17:56:58 +0400

    fix: change mini.nvim URL to recommended from deprecated

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ return {
     vim.pack.add({
         "https://github.com/folke/tokyonight.nvim",
         "https://github.com/folke/which-key.nvim",
-        "https://github.com/echasnovski/mini.nvim",
+        "https://github.com/nvim-mini/mini.nvim",
         {
             src = "https://github.com/nvim-treesitter/nvim-treesitter",
             version = "main",
