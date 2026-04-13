bc9a30541252b312f722c258f2e0e8ce57e08b83
Harrier du Bois <sadyukh@gmail.com>
2026-04-13 20:07:45 +0400

    fix: specific mini.nvim modules instead of the whole bundle

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,8 @@ return {
     vim.pack.add({
         "https://github.com/folke/tokyonight.nvim",
         "https://github.com/folke/which-key.nvim",
-        "https://github.com/nvim-mini/mini.nvim",
+        "https://github.com/echasnovski/mini.diff",
+        "https://github.com/echasnovski/mini.completion",
         {
             src = "https://github.com/nvim-treesitter/nvim-treesitter",
             version = "main",
